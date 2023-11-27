42938c42f4c1c770112190ddfb5959384535b8bf
Mike Fulcher <mike@pocketsmith.com>
2023-11-28 10:50:41 +1300

    Remove Rails 3+ initializers
    The engine simply added controller helpers which is trivial to do manually.
    
    The railtie was just there to defer configuring Split until after other gems have been loaded and initialised, which isn't essential.

--- lib/split.rb
+++ lib/split.rb
@@ -24,7 +24,6 @@ require "split/trial"
 require "split/user"
 require "split/version"
 require "split/zscore"
-require "split/engine" if defined?(Rails)
 
 module Split
   extend self
@@ -73,11 +72,4 @@ module Split
   end
 end
 
-# Check to see if being run in a Rails application.  If so, wait until before_initialize to run configuration so Gems that create ENV variables have the chance to initialize first.
-if defined?(::Rails)
-  class Split::Railtie < Rails::Railtie
-    config.before_initialize { Split.configure { } }
-  end
-else
-  Split.configure { }
-end
+Split.configure { }
